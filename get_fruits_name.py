58a39084dab973a85c9db4a1efb16440736ca21e
djumanov <diyorbekjumanov1120@gmail.com>
2022-11-11 20:15:14 +0500

    done

---
+++ get_fruits_name.py
@@ -0,0 +1,15 @@
+def get_frutis_name(data:str)->list:
+    """
+    This function returns the name of the fruits in the list
+
+    args:
+        data: CSV file with the fruits data
+    returns:
+        list of fruits names
+    """
+
+data = open('fruits.csv').read()
+
+print(get_frutis_name(data))
+
+    
