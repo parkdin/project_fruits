4c3b4f3a1c017c9d4f95812c88f54a0d4e220f91
parkdin <abdukokhorovizatillo00@gmail.com>
2022-11-16 05:06:35 +0900

    done

--- get_total_price.py
+++ get_total_price.py
@@ -7,3 +7,9 @@ def get_total_price(data:str)->float:
     returns:
         list of fruits total price
     """
+    sum = 0
+    for i in  data.split()[1:]:
+        sum += float(i.split(',')[1])
+    return sum
+data = open('fruits.csv').read()
+print(get_total_price(data))
